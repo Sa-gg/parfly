7960595c6ac018a4c8f6da48489086f94e9174df
jegs-sildora <jegrick.sildora@chmsu.edu.ph>
2024-11-30 09:02:09 +0800

    my first commit

---
+++ login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible"
+        content="ie=edge">
+  <script src='https://cdn.tailwindcss.com'></script>
+	<link rel='apple-touch-icon' sizes='180x180' href='./favicon/apple-touch-icon.png'>
+	<link rel='icon' type='image/png' sizes='32x32' href='./favicon/favicon-32x32.png'>
+	<link rel='icon' type='image/png' sizes='16x16' href='./favicon/favicon-16x16.png'>
+	<link rel='manifest' href='./favicon/site.webmanifest'>
+	<title>Log In</title>
+</head>
+<body>
+	<div class="flex h-screen">
+		<div class="w-1/3">
+			
+		</div>
+		<div class="w-2/3"></div>
+	</div>
+</body>
+</html>
